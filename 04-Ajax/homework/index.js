2f5e1066065a6a3bc97cdfe07a1d924ca1f603eb
victorwake <victorpintowake@gmail.com>
2022-09-16 21:29:36 -0300

    ajax

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,151 +1,74 @@
 //Para manipular un elemento de html en con jquery debes usar $('#id-elemento') 
 //agregandole como arguemnto el id del elemento que quieres manipular
 
+const createListItem = (response) => {
+    response.forEach(element => {
+      $(`<li>${element.name}</li>`).appendTo('#lista')
+    })
+  }
+
+    function mostrarAmigos() {
+    $.ajax({
+      url: `http://localhost:5000/amigos`,
+      type: "get",
+      success: (info) => {
+        info.forEach((amigo) => $("#lista").append(`<li>${amigo.name}</li>`));
+      },
+    });
+    
+  }
+
+  $('#boton').click(() => {
+    $('#angel').hide();
+  });
+
+  
+  const searchElement = (response) => {
+    $('#amigo').text(response.name)
+  }
+  
+  $('#boton').click(() => {
+    $('#lista').empty()
+    $.get('http://localhost:5000/amigos', createListItem)
+  })
+  
+  $('#search').click(() => {
+
+    const id = $('#input')[0].value
+    limpiar("input")
+    if(id < 7) {
+    $.get(`http://localhost:5000/amigos/${id}`, searchElement)
+    } else {
+      alert('El id no existe')
+    }
+  })
+  
+
+  $('#delete').click(() => {
+    const id = $('#inputDelete')[0].value
+    $.ajax({
+      url: `http://localhost:5000/amigos/${id}`,
+      type: 'DELETE',
+      success: (response) => {
+        $('#lista').empty()
+        alert('AMIGO ELIMINADO')
+        createListItem(response)
+      }
+    })
+  })
+
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+function limpiar(campo) {
+    if (campo === " contenedor") {
+      $("#amigo").empty();
+      $("#lista").empty();
+    } else if (campo === "input") {
+      $("#input").val("");
+      $("#inputDelete").val("");
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function limpiar(campo) {
-//     if (campo === " contenedor") {
-//       $("#amigo").empty();
-//       $("#lista").empty();
-//     } else if (campo === "input") {
-//       $("#input").val("");
-//       $("#inputDelete").val("");
-//     }
-//   }
-
-//   function mostrarAmigos() {
-//     $.ajax({
-//       url: `http://localhost:5000/amigos`,
-//       type: "get",
-//       success: (info) => {
-//         info.forEach((amigo) => $("#lista").append(`<li>${amigo.name}</li>`));
-//       },
-//     });
-//   }
-
-//   function buscarAmigo(id) {
-//     $.ajax({
-//       url: `http://localhost:5000/amigos/${id}`,
-//       type: "get",
-//       success: (info) => {
-//         $("#amigo").append(`<li>${info.name}</li>`);
-//         $("#amigo").append(`<li>${info.email}</li>`);
-//         limpiar("input");
-//         //mostrarAmigos();
-//       },
-//       error: (error) => {
-//         if (error) {
-//           alert("Id no valido");
-//           limpiar("input");
-//         }
-//       },
-//     });
-//   }
-
-//   function borrarAmigo(id) {
-//     $.ajax({
-//       url: `http://localhost:5000/amigos/${id}`,
-//       type: "delete",
-//       success: (info) => {
-//         alert("Amigo eliminado con exito");
-//         limpiar();
-//         mostrarAmigos();
-//       },
-//     });
-//   }
-
-//   $("#boton").click(() => {
-//     limpiar("contenedor"); //este comando limpia la lista
-//     //  console.log("ok");
-//     mostrarAmigos();
-//   });
-
-//   $("#search").click(() => {
-//     let id = $("#input").val();
-//     if (!id) return alert("Ingrese un id");
-//     limpiar("contenedor");
-//     buscarAmigo(id);
-//   });
-
-//   $("#delete").click(() => {
-//     let id = $("#inputDelete").val();
-//     if (!id) return alert("Ingrese un id");
-//     borrarAmigo(id);
-//   });
-
-//   $("enviarForm").click(() => {
-//     let nombre = $("#name").val();
-//     let age = $("#age").val();
-//     let email = $("#email").val();
-//     if (!nombre || !age || !email)
-//       return alert("Ingrese un nombre, edad y email");
-//     $.ajax({
-//       url: `http://localhost:5000/amigos`,
-//       type: "post",
-//       data: { name: nombre, age: age, email: email },
-//       success: (info) => {
-//         alert("Amigo agregado con exito");
-//         limpiar("input");
-//         mostrarAmigos();
-//       },
-//     });
-//   });
