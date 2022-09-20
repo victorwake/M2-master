739997acae5dc3c9a2590a52c1878b86060666ed
victorwake <victorpintowake@gmail.com>
2022-09-19 21:52:07 -0300

    ajax terminado

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,74 +1,70 @@
 //Para manipular un elemento de html en con jquery debes usar $('#id-elemento') 
 //agregandole como arguemnto el id del elemento que quieres manipular
 
-const createListItem = (response) => {
-    response.forEach(element => {
-      $(`<li>${element.name}</li>`).appendTo('#lista')
-    })
+function limpiar(campo) {
+  if (campo === "contenedor") {
+    $("#amigo").empty();
+    $("#lista").empty();
+  } else if (campo === "input") {
+    $("#input").val("");
+    $("#inputDelete").val("");
   }
+}
 
-    function mostrarAmigos() {
-    $.ajax({
-      url: `http://localhost:5000/amigos`,
-      type: "get",
-      success: (info) => {
-        info.forEach((amigo) => $("#lista").append(`<li>${amigo.name}</li>`));
-      },
-    });
-    
-  }
-
-  $('#boton').click(() => {
-    $('#angel').hide();
-  });
-
-  
-  const searchElement = (response) => {
-    $('#amigo').text(response.name)
-  }
-  
-  $('#boton').click(() => {
-    $('#lista').empty()
-    $.get('http://localhost:5000/amigos', createListItem)
-  })
-  
-  $('#search').click(() => {
-
-    const id = $('#input')[0].value
-    limpiar("input")
-    if(id < 7) {
-    $.get(`http://localhost:5000/amigos/${id}`, searchElement)
-    } else {
-      alert('El id no existe')
+const amigosLista = function (){
+  let lista = $("#lista");
+  lista.empty();
+  $.get('http://localhost:5000/amigos', response => {
+    for(let i = 0; i < response.length; i++) {
+      let cross = `<button onclick="deleteFriends(${response[i].id})">X</button>`;
+      $('#lista').append(`<li>${"id: " + response[i].id + " " + response[i].name + " " + cross}</li>`)
     }
   })
-  
+}
 
-  $('#delete').click(() => {
-    const id = $('#inputDelete')[0].value
-    $.ajax({
-      url: `http://localhost:5000/amigos/${id}`,
+$('#boton').click(() => {
+  amigosLista();
+})   //funcion para mostrar la lista de amigos
+
+$('#boton').click(() => {
+$('#imagenLoadin').hide();
+}); //Limpia la imagen de carga
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Buscar un amigo por id
+$('#search').click(() => {
+  // cont = itemLength();
+  // console.log(cont);
+  const id = $('#input')[0].value
+  limpiar("input")
+  if(id < 7) {
+  $.get(`http://localhost:5000/amigos/${id}`, response => {
+    $('#amigo').text(response.name);
+    });
+  }else{
+    alert("Indice invalido")
+  }
+});
+  
+////////////////////////////////////////////////////////////////////////////////////////
+//Eliminar un amigo por id
+const deleteFriends = function(idFriend){
+  $.ajax({
+    url: `http://localhost:5000/amigos/${idFriend}`,
       type: 'DELETE',
-      success: (response) => {
-        $('#lista').empty()
-        alert('AMIGO ELIMINADO')
-        createListItem(response)
+      success:() =>{
+        $('#success').text('Eliminado con exito');
+        amigosLista();
       }
     })
-  })
+}
+
+$('#delete').click(() => {
+  let id = $('#inputDelete').val();
+  deleteFriends(id);
+});  
 
 
 
-////////////////////////////////////////////////////////////////////////
-
-
-function limpiar(campo) {
-    if (campo === " contenedor") {
-      $("#amigo").empty();
-      $("#lista").empty();
-    } else if (campo === "input") {
-      $("#input").val("");
-      $("#inputDelete").val("");
-    }
-  }
 
